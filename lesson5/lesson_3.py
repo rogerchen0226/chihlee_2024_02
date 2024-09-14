81d9f4522ebd540da42cc0253237787ff15866e8
RogerChen <28897537+rogerchen0226@users.noreply.github.com>
2024-09-14 14:41:01 +0800

    package

--- lesson5/lesson_3.py
+++ lesson5/lesson_3.py
@@ -1,16 +1,16 @@
-import tools # 匯入tools.py檔案
+import widget # 匯入tools.py檔案
 
 while True:
     kg = 0  # 清除變數
     cm = 0  # 清除變數
-    cm, kg = tools.input_data()  # 呼叫function
+    cm, kg = widget.input_data()  # 呼叫function
 
     print(f'身高={cm},體重={kg}')
 
-    BMI = tools.calculate_bmi(cm1=cm, kg1=kg)
+    BMI = widget.calculate_bmi(cm1=cm, kg1=kg)
 
     print(f'BMI={BMI}')
-    print(tools.get_status(BMI))
+    print(widget.get_status(BMI))
 
     play_again = input("還要繼續嗎?(y,n)")
     if play_again == "n":
