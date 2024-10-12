b631414fdd4a050383476a5b8b88152bf4609f2c
RogerChen <28897537+rogerchen0226@users.noreply.github.com>
2024-10-12 16:18:17 +0800

    led&temp

--- lesson13/computer/index.py
+++ lesson13/computer/index.py
@@ -1,7 +1,27 @@
 import paho.mqtt.client as mqtt
+from datetime import datetime
+import os,csv
 
-def record():
-    pass
+def record(r):
+    root_dir = os.getcwd()
+    data_dir = os.path.join(root_dir, 'data')
+    if not os.path.isdir(data_dir):    
+            os.mkdir('data')
+    
+    today = datetime.today()
+    filename = today.strftime("%Y-%m-%d") + ".csv"
+    #get_file_abspath
+    full_path = os.path.join(data_dir,filename)
+    if not os.path.exists(full_path):
+        #沒有這個檔,建立檔案
+        print('沒有這個檔')
+        with open(full_path,mode='w',encoding='utf-8',newline='') as file:
+            file.write('時間,設備,值\n')
+    
+    with open(full_path, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(r)
+
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print("Connected with result code " + str(properties))
@@ -10,15 +30,28 @@ def on_connect(client, userdata, flags, reason_code, properties):
 
 def on_message(client, userdata, msg):
     global led_origin_value # 宣告全域變數
+    global temp_origin_value # 宣告全域變數
     topic = msg.topic
     value = msg.payload.decode()
     if topic == "SA-21/LED_LEVEL":
         led_value = int(value)
         if led_value != led_origin_value:
             led_origin_value = led_value
-            record()
             print(f"led_value '{led_value}' ")
+            today = datetime.today()
+            now_str = today.strftime('%Y-%m-%d %H:%M:%S')
+            save_str = [now_str, topic,led_value]
+            record(save_str)
 
+    if topic == "SA-21/TEMP":
+        temp_value = float(value)
+        if temp_value != temp_origin_value:
+            temp_origin_value = temp_value
+            print(f"temp_value '{temp_value}' ")
+            today = datetime.today()
+            now_str = today.strftime('%Y-%m-%d %H:%M:%S')
+            save_str = [now_str, topic,temp_value]
+            record(save_str)
 
     #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
 
@@ -36,4 +69,5 @@ def main():
 
 if __name__ == "__main__":
     led_origin_value = 0
+    temp_origin_value = 0.0
     main()
