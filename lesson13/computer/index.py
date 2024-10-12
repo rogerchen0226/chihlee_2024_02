d794af77cfd9d11cdc39a9e0dde175254152bef0
RogerChen <28897537+rogerchen0226@users.noreply.github.com>
2024-10-12 15:53:18 +0800

    update

--- lesson13/computer/index.py
+++ lesson13/computer/index.py
@@ -1,11 +1,26 @@
 import paho.mqtt.client as mqtt
 
+def record():
+    pass
+
 def on_connect(client, userdata, flags, reason_code, properties):
+    print("Connected with result code " + str(properties))
     #連線bloker成功時,只會執行一次
     client.subscribe("SA-21/#")
 
 def on_message(client, userdata, msg):
-    print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+    global led_origin_value # 宣告全域變數
+    topic = msg.topic
+    value = msg.payload.decode()
+    if topic == "SA-21/LED_LEVEL":
+        led_value = int(value)
+        if led_value != led_origin_value:
+            led_origin_value = led_value
+            record()
+            print(f"led_value '{led_value}' ")
+
+
+    #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
 
 def main():
     client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
@@ -20,4 +35,5 @@ def main():
 
 
 if __name__ == "__main__":
+    led_origin_value = 0
     main()
