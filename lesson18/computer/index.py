607a623aee9e484ab9e17b127be6bd52c19a835c
RogerChen <28897537+rogerchen0226@users.noreply.github.com>
2024-10-26 11:42:36 +0800

    update

---
+++ lesson18/computer/index.py
@@ -0,0 +1,96 @@
+import paho.mqtt.client as mqtt
+from datetime import datetime
+import os,csv
+
+
+def record(date:str,topic:str,value:str):
+    '''
+    # 檢查是否有data資料夾,如果沒有就建立data資料夾
+    # 檢查是否有今天的檔案,如果沒有今天日期的.csv就建立今天日期的.csv,並寫入資料
+    # parameters date:str -> 日期
+    #            topic:str -> 主題
+    #            value:str -> 值
+    '''
+    root_dir = os.getcwd()
+    data_dir = os.path.join(root_dir, 'data')
+    if not os.path.isdir(data_dir):    
+            os.mkdir('data')
+    
+    today = datetime.today()
+    filename = today.strftime("%Y-%m-%d") + ".csv"
+    #get_file_abspath
+    full_path = os.path.join(data_dir,filename)
+    if not os.path.exists(full_path):
+        #沒有這個檔,建立檔案
+        print('沒有這個檔')
+        with open(full_path,mode='w',encoding='utf-8',newline='') as file:
+            file.write('時間,設備,值\n')
+    
+    with open(full_path, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([date,topic,value])
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print("Connected with result code " + str(properties))
+    #連線bloker成功時,只會執行一次
+    client.subscribe("SA-21/#")
+    
+
+def on_message(client, userdata, msg):
+    global led_origin_value # 宣告全域變數
+    global temp_origin_value # 宣告全域變數
+    global light_origin_value # 宣告全域變數
+
+    topic = msg.topic
+    value = msg.payload.decode()
+    #print(f"topic '{topic}' value '{value}' type '{type(value)}'")
+    if topic == "SA-21/LED_LEVEL":
+        led_value = int(value)
+        if led_value != led_origin_value:
+            led_origin_value = led_value
+            print(f"led_value '{led_value}' ")
+            today = datetime.today()
+            now_str = today.strftime('%Y-%m-%d %H:%M:%S')
+            #save_str = [now_str, topic,led_value]
+            record(now_str,topic,led_value)
+
+    if topic == "SA-21/TEMP":
+        temp_value = "{:.2f}".format(round(float(value),ndigits=2))
+        if temp_value != temp_origin_value:
+            temp_origin_value = temp_value
+            print(f"temp_value '{temp_value}' ")
+            today = datetime.today()
+            now_str = today.strftime('%Y-%m-%d %H:%M:%S')
+            #save_str = [now_str, topic,temp_value]
+            record(now_str,topic,temp_value)
+
+    if topic == "SA-21/LIGHT":
+        light_value = int(value)
+        if light_value != light_origin_value:
+            light_origin_value = light_value
+            print(f"light_value '{light_value}' ")
+            today = datetime.today()
+            now_str = today.strftime('%Y-%m-%d %H:%M:%S')
+            #save_str = [now_str, topic,temp_value]
+            record(now_str,topic,light_value)
+
+    #print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
+
+def main():
+    client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+    # 設定用戶名和密碼
+    username = "pi"  # 替換為您的用戶名
+    password = "raspberry"  # 替換為您的密碼
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_message = on_message 
+    client.connect("192.168.0.252", 1883, 60)
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    led_origin_value = 0
+    temp_origin_value = 0.0
+    light_origin_value = 0
+    main()
---
+++ lesson18/main.py
@@ -0,0 +1,78 @@
+#! usr/bin/micropython
+
+'''
+led->gpio15
+光敏電阻 -> gpio28
+可變電阻 -> gpio26
+內建溫度sensor -> adc最後1pin,共5pin
+'''
+
+from machine import Timer,ADC,Pin,PWM,RTC
+import binascii
+from umqtt.simple import MQTTClient
+import tools,config
+
+
+def do_thing(t):
+    '''
+    :param t:Timer的實體
+    負責偵測溫度和光線
+    每2秒執行1次
+    '''
+    conversion_factor = 3.3 / (65535)
+    reading = adc.read_u16() * conversion_factor
+    temperature = round(27 - (reading - 0.706)/0.001721,2)
+    formatted_number = "{:.2f}".format(temperature)
+    print(f'溫度:{formatted_number}')
+    mqtt.publish('SA-21/TEMP', f'{formatted_number}')
+    blynk_mqtt.publish('ds/temp_level',f'{formatted_number}')
+    adc_value = adc_light.read_u16()
+    #adc_value = (adc_value // 1000) * 1000 # 固定尾碼為000
+    line_state = 0 if adc_value < 10000 else 1
+    print(f'光線:{line_state}')
+    mqtt.publish('SA-21/LIGHT', f'{line_state}')
+    blynk_mqtt.publish('ds/light_level', f'{line_state}')
+    
+    
+def do_thing1(t):
+    '''
+    :param t:Timer的實體
+    負責可變電阻和改變led的亮度
+    '''
+    
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty)
+    led_level = round(duty/65535*10)
+    print(f'可變電阻:{led_level}')
+    mqtt.publish('SA-21/LED_LEVEL', f'{led_level}')
+    blynk_mqtt.publish('ds/led_level', f'{led_level}')
+
+def main():
+    try:
+        tools.connect()
+        print(config.BLYNK_MQTT_BROKER)
+        print(config.BLYNK_TEMPLATE_ID)
+        print(config.BLYNK_AUTH_TOKEN)
+        blynk_mqtt.connect()
+        mqtt.connect()
+    except RuntimeError as e:
+        print(e)
+    except Exception:
+        print('不知明的錯誤')
+    else:
+        t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+        t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+        
+
+if __name__ == '__main__':
+    adc = ADC(4) #內建溫度
+    adc1 = ADC(Pin(26)) #可變電阻
+    adc_light = ADC(Pin(28)) #光敏電阻
+    pwm = PWM(Pin(15),freq=50) #pwm led
+    
+    #MQTT
+    SERVER = "192.168.0.252"
+    CLIENT_ID = binascii.hexlify(machine.unique_id())
+    mqtt = MQTTClient(CLIENT_ID, SERVER,user='pi',password='raspberry')
+    blynk_mqtt = MQTTClient(config.BLYNK_TEMPLATE_ID, config.BLYNK_MQTT_BROKER,user='device',password=config.BLYNK_AUTH_TOKEN)
+    main()
